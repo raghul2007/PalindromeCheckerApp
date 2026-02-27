bd0deaf60893319a7e8ac57b7c08e972226160f6
your GitHub username <your GitHub email>
2026-02-27 16:40:17 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,13 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // UC1 - Welcome Message
+        System.out.println("======================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println(" Version : 1.0");
+        System.out.println("======================================");
+        System.out.println("Application initialized successfully.");
+
+    }
 }
